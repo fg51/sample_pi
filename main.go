260b0e628d7bd93301bfcc133187881bbf114fa0
kflange <zknsftk1msrk@gmail.com>
2017-11-21 14:41:05 +0900

    show result pi

--- main.go
+++ main.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"fmt"
+	"math/cmplx"
 	"math/rand"
 	"time"
+
 	"./gencmplx"
 )
 
 
 func main() {
 	c := gencmplx.New(rand.NewSource(time.Now().UnixNano()), int64(10000))
+	total := int64(0)
+	in_circle := int64(0)
 	for i := range c {
-		fmt.Printf("%v\t%v\n", real(i), imag(i))
+		total++
+		if cmplx.Abs(i) <= float64(1) {
+			in_circle++
+		}
 	}
+	fmt.Printf("total = %v, hit = %v, pi = %v \n", total, in_circle, float64(4 * in_circle) / float64(total))
 }
